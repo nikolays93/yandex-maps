9e14d23168d9658997043a1bb58a08dfa99f938c
nikolays93 <nikolays93@ya.ru>
2019-10-08 09:51:09 +0400

    Insert maps to collection

--- public.js
+++ public.js
@@ -1,54 +1,57 @@
 
-(function(w) {
-    w.WPYandexMap = function( handle, args ) {
-        this.placemarks = [];
-
-        this.args = {
-            center: null,
-            zoom: null,
-            controls: []
-        };
-
-        if( args.center )   this.args.center = this.escCoords( args.center );
-        if( args.zoom )     this.args.zoom = parseInt( args.zoom );
-        if( args.controls ) this.args.controls = args.controls;
-
-        this.mapInstance = new ymaps.Map(handle, this.args);
+window.WPYandexMap = function( handle, args ) {
+    this.args = {
+        center: null,
+        zoom: null,
+        controls: []
     };
 
-    w.WPYandexMap.prototype.escCoords = function( coords ) {
-        if( 'string' == typeof(coords) ) {
-            coords.split(':');
+    if( args.center )   this.args.center = this.escCoords( args.center );
+    if( args.zoom )     this.args.zoom = parseInt( args.zoom );
+    if( args.controls ) this.args.controls = args.controls;
+
+    this.mapInstance = new ymaps.Map(handle, this.args);
+
+    this.placemarks = [];
+};
+
+window.WPYandexMap.prototype.escCoords = function( coords ) {
+    if( 'string' == typeof(coords) ) {
+        coords.split(':');
+    }
+
+    return coords;
+};
+
+window.WPYandexMap.prototype.insertPlacemark = function( placemark ) {
+    placemark = placemark || {};
+    let coords = this.escCoords( placemark.coords || false );
+    let bullet = {};
+    let options = {};
+    var newPlacemark = false;
+
+    if( coords ) {
+        if( placemark.title )  bullet.balloonContentHeader = placemark.title;
+        if( placemark.body )   bullet.balloonContentBody   = placemark.body;
+        if( placemark.footer ) bullet.balloonContentFooter = placemark.footer;
+        if( placemark.color )  options.iconColor           = placemark.color;
+
+        var newPlacemark = new ymaps.Placemark(coords, bullet, options);
+
+        this.placemarks.push( newPlacemark );
+        this.mapInstance.geoObjects.add( newPlacemark );
+
+        if( placemark.opened && (placemark.title || placemark.body || placemark.footer) ) {
+            newPlacemark.balloon.open();
         }
+    }
 
-        return coords;
-    };
 
-    w.WPYandexMap.prototype.insertPlacemark = function( placemark ) {
-        placemark = placemark || {};
-        let coords = this.escCoords( placemark.coords || false );
-        let bullet = {};
-        let options = {};
-        var newPlacemark = false;
 
-        if( coords ) {
-            if( placemark.title )  bullet.balloonContentHeader = placemark.title;
-            if( placemark.body )   bullet.balloonContentBody   = placemark.body;
-            if( placemark.footer ) bullet.balloonContentFooter = placemark.footer;
+    return newPlacemark;
+};
 
-            if( placemark.color ) options.iconColor           = placemark.color;
-
-            var newPlacemark = new ymaps.Placemark(coords, bullet, options);
-
-            this.mapInstance.geoObjects.add( newPlacemark );
-            if( placemark.opened && (placemark.title || placemark.body || placemark.footer) ) {
-                newPlacemark.balloon.open();
-            }
-        }
-
-        return newPlacemark;
-    };
-})(window);
+var yandexMapsCollection = {};
 
 if( typeof yandex_maps !== "undefined" ) {
     ymaps.ready(function() {
@@ -62,6 +65,8 @@ if( typeof yandex_maps !== "undefined" ) {
             Object.keys(placemarks || []).map(function(placemarkKey, index) {
                 WPYMap.insertPlacemark( placemarks[placemarkKey] );
             });
+
+            yandexMapsCollection[mapKey] = WPYMap;
         });
     });
 }
